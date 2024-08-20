e4f9c5fad274fe9b389f1ee154cca30bdd0b2b43
sanggusseu <sanggusseu@gmail.com>
2024-08-20 16:50:37 +0900

    MainContent tab 설정

---
+++ src/layouts/MainContent.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <q-tabs
+      v-model="tab"
+      dense
+      class="bg-white text-grey-7"
+      active-color="primary"
+      indicator-color="primary"
+      align="start"
+    >
+      <q-tab name="about" label="About" class="text-capitalize" />
+      <q-tab name="projects" label="Projects" class="text-capitalize" />
+      <q-tab name="skills" label="Skills" class="text-capitalize" />
+      <q-tab name="etc" label="Etc" class="text-capitalize" />
+    </q-tabs>
+
+    <q-tab-panels v-model="tab" animated class="bg-white text-dark">
+      <q-tab-panel name="about">
+        <div class="text-h6">About</div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </q-tab-panel>
+
+      <q-tab-panel name="projects">
+        <div class="text-h6">Projects</div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </q-tab-panel>
+
+      <q-tab-panel name="skills">
+        <div class="text-h6">Skills</div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </q-tab-panel>
+
+      <q-tab-panel name="etc">
+        <div class="text-h6">Etc</div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </q-tab-panel>
+    </q-tab-panels>
+  </section>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const tab = ref('projects');
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,13 @@
 <template>
-  <q-page>
-    <TheFooter></TheFooter>
+  <q-page class="q-py-xl q-px-lg">
+    <MainContent></MainContent>
   </q-page>
+  <TheFooter></TheFooter>
 </template>
 
 <script setup>
 import TheFooter from 'src/layouts/TheFooter.vue';
+import MainContent from 'src/layouts/MainContent.vue';
 
 defineOptions({
   name: 'IndexPage',
