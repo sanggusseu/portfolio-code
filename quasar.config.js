d9faf7982ac9f4df190f1e03f80561550209f0b7
sanggusseu <sanggusseu@gmail.com>
2024-08-21 17:25:51 +0900

    publicPath 메인으로 수정

--- quasar.config.js
+++ quasar.config.js
@@ -51,7 +51,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/portfolio-code',
+      publicPath: '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
