bbb6d12b5f8b008249316f1ca7dd2d0892112155
sanggusseu <sanggusseu@gmail.com>
2024-08-21 12:30:17 +0900

    Header & Drawer 설정

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header elevated class="bg-white text-dark">
       <q-toolbar>
         <q-btn
           flat
@@ -11,29 +11,29 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title class="title text-weight-bold"
+          >황상은 포트폴리오</q-toolbar-title
+        >
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" bordered>
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+        <q-item-label header class="text-h6"> Social Links </q-item-label>
 
         <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
+          v-for="social in socialList"
+          :key="social.title"
+          v-bind="social"
+        />
+      </q-list>
+      <q-list>
+        <q-item-label header class="text-h6"> Project Links </q-item-label>
+
+        <EssentialLink
+          v-for="project in projectList"
+          :key="project.title"
+          v-bind="project"
         />
       </q-list>
     </q-drawer>
@@ -45,61 +45,68 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { ref } from 'vue';
+import EssentialLink from 'components/EssentialLink.vue';
 
 defineOptions({
-  name: 'MainLayout'
-})
+  name: 'MainLayout',
+});
 
-const linksList = [
+const socialList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
+    title: '깃허브',
+    caption: 'sanggusseu 깃허브로 이동',
     icon: 'code',
-    link: 'https://github.com/quasarframework'
+    link: 'https://github.com/sanggusseu',
   },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+];
 
-const leftDrawerOpen = ref(false)
+const projectList = [
+  {
+    title: '명언 제조기',
+    caption: '명언처럼 보이게 만들어 주는 사이트',
+    icon: 'edit',
+    link: 'https://sanggusseu.github.io/make-quote/',
+  },
+  {
+    title: '멋진 헛간',
+    caption: '유용한 사이트 모음',
+    icon: 'star',
+    link: 'https://sanggusseu.github.io/make-quote/',
+  },
+  {
+    title: 'Boggle',
+    caption: '검색 사이트',
+    icon: 'search',
+    link: 'https://sanggusseu.github.io/boggle/',
+  },
+  {
+    title: 'CO2 Table Maker',
+    caption: '맞춤형 CO2 Table을 만들어주는 사이트',
+    icon: 'waves',
+    link: 'https://sanggusseu.github.io/co2table-maker/',
+  },
+];
 
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
 }
 </script>
+
+<style lang="scss" scoped>
+.title {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+
+@media (min-width: $breakpoint-sm) {
+  .title {
+    position: static;
+    transform: none;
+  }
+}
+</style>
