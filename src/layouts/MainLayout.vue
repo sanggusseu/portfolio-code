8c92ad098700f625638e0d24e0786021d7dafae3
sanggusseu <sanggusseu@gmail.com>
2024-09-10 12:25:25 +0900

    About Tab 내용 추가

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated class="bg-white text-dark">
+    <q-header unelevated bordered class="bg-white text-dark">
       <q-toolbar>
         <q-btn
           flat
